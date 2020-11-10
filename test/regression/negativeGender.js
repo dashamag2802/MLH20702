543e31e55922ff51c9b8188a1d2ad306e5713cee
Aleks9960521 <aleks9960521@gmail.com>
2020-11-10 12:24:23 -0500

    change in negative Gender

---
+++ test/regression/negativeGender.js
@@ -0,0 +1,45 @@
+const sel = require ('../../data/selectors.json');
+const data = require ('../../data/testData.json');
+
+describe('User cant choose more than 1 option', function () {
+
+    it('A-114 user cant select he and she together', function () {      //TC-027
+        browser.url('https://qa-apps.netlify.app/app_my_hero');
+        const input1 = $$(sel.genderTypeK)[data.gender.he[0]].click();
+        const input2 = $$(sel.genderTypeK)[data.gender.she[0]].click();
+        const sheSelected = $$(sel.genderTypeK)[data.gender.she[0]].isSelected();
+        const heSelected = $$(sel.genderTypeK)[data.gender.he[0]].isSelected();
+        expect(sheSelected).toEqual(true);
+        expect(heSelected).toEqual(false);
+    });
+
+    it('A-115 user cant select he and it together', function () {      //TC-028
+        const input1 = $$(sel.genderTypeK)[data.gender.he[0]].click();
+        const input2 = $$(sel.genderTypeK)[data.gender.it[0]].click();
+        const heSelected = $$(sel.genderTypeK)[data.gender.he[0]].isSelected();
+        const itSelected = $$(sel.genderTypeK)[data.gender.it[0]].isSelected();
+        expect(itSelected).toEqual(true);
+        expect(heSelected).toEqual(false);
+    });
+
+    it('A-116 user cant select she and it together', function () {      //TC-029
+        const input1 = $$(sel.genderTypeK)[data.gender.she[0]].click();
+        const input2 = $$(sel.genderTypeK)[data.gender.it[0]].click();
+        const sheSelected = $$(sel.genderTypeK)[data.gender.she[0]].isSelected();
+        const itSelected = $$(sel.genderTypeK)[data.gender.it[0]].isSelected();
+        expect(itSelected).toEqual(true);
+        expect(sheSelected).toEqual(false);
+    });
+    it('A-117 user cant select he, she and it together', function () {      //TC-030
+        const input1 = $$(sel.genderTypeK)[data.gender.he[0]].click();
+        const input2 = $$(sel.genderTypeK)[data.gender.she[0]].click();
+        const input3 = $$(sel.genderTypeK)[data.gender.it[0]].click();
+        const heSelected = $$(sel.genderTypeK)[data.gender.he[0]].isSelected();
+        const sheSelected = $$(sel.genderTypeK)[data.gender.she[0]].isSelected();
+        const itSelected = $$(sel.genderTypeK)[data.gender.it[0]].isSelected();
+        expect(sheSelected).toEqual(false);
+        expect(heSelected).toEqual(false);
+        expect(itSelected).toEqual(true);
+    });
+
+});
