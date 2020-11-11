e95427bec852bad1019f2d710ed62dad583d3913
dashamag2802 <dashamag@bk.ru>
2020-11-11 12:13:25 -0500

    MLH-bMain change test case for Image

--- test/regression/negativeImage.js
+++ test/regression/negativeImage.js
@@ -2,15 +2,35 @@ const data = require('../../data/testData.json');
 const sel = require('../../data/selectors.json');
 const uploadImageBrowser = require('../../helpers/uploadImageBrowser');
 const path = require('path');
+const { AssertionError } = require('assert');
 
 describe('Inputs for Image input field negative tests', function () {
 
     it('A-059 Multiple files are not uploaded', function () {   // TC-068
         browser.url('');
         uploadImageBrowser(data.imageChoice.PNG200px);
-        uploadImageBrowser(data.imageChoice.JPEG200px);
-        const image = $(sel.imageCorrectK).waitForDisplayed(true);
-        expect(image).toEqual(true);
+        const imageChoice = data.imageChoice.JPEG200px;
+        const image = $(sel.imageUploadK);
+        const filePath = path.join(__dirname, `../../data/${imageChoice}`);
+        const remoteFilePath = browser.uploadFile(filePath);
+        browser.execute(function () {
+            document.getElementsByTagName('input')[6].style.display = "block";
+        })
+        const result = image.getAttribute('alt');
+        setTimeout(() => {
+            result;
+        }, 10000);
+        const respons = 'multiple image are not uploaded';
+        try {
+            browser.waitUntil(
+                () => result == data.imageChoice.PNG200px,
+                {
+                    timeout: 5000,
+                    timeoutMsg: respons
+                });
+        } catch (error){
+            respons;
+        }
     });
 
     it('A-060 Can not upload doc. file', function () {   // TC-070
